a95d2e76a0f292282e027e1537a9fe191f17903b
kenmine172 <k.1990.0105@icloud.com>
2021-05-13 09:21:11 +0900

    lssue2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_Week
     @plan = Plan.new
   end
 
@@ -12,7 +12,7 @@ class CalendarsController < ApplicationController
     redirect_to action: :index
   end
 
-  private
+  Private
 
   def plan_params
     params.require(:calendars).permit(:date, :plan)
